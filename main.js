30c1db98703156b701b4671bf74347571bf49f47
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2023-02-07 20:41:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ objects=[];
 
 
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(360,360);
     canvas.center();
+video=createCapture(VIDEO);
+video.size(360,360);
+video.hide()
+}
 
+function start(){
     objectDetector=ml5.objectDetector('cocossd',modelloaded);
     document.getElementById("status").innerHTML="status:detecting objects"
 }
+
 
 
 function modelloaded(){
@@ -35,24 +41,29 @@ function gotResult(error,results){
 }
 
 function preload(){
-    img=loadImage("tv.jpg")
+    img=loadImage("dog_cat.jpg")
 
 }
 
 function draw(){
-    image(img,0,0,640,420);
+    image(video,0,0,360,360);
     
    
 
     if(status != ""){
+        objectDetector.detect(video,gotResult)
+        r=random(255);
+        g=random(255);
+        b=random(255);
         for(i=0; i<objects.length; i++){
             document.getElementById("status").innerHTML="status:objects detected";
-            fill("#d66d0b");
+            document.getElementById("numbers_of_object").innerHTML="numbers of objects detected are : "+objects.length;
+            fill(r,g,b);
 
             percent=floor(objects[i].confidence*100);
             text(objects[i].label+" "+percent+"%",objects[i].x+20,objects[i].y+20);
             noFill();
-            stroke("#d66d0b");
+            stroke(r,g,b);
             rect(objects[i].x,objects[i].y,objects[i].width ,objects[i].height);
         
         }
